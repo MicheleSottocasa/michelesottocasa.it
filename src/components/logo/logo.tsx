0d64d0d65a0b36da6e47543a8e3398a2a38d418a
em--global ail contact@michelesottocasa.it <michelesottocasa1@gmail.com>
2023-11-08 23:48:12 +0100

    Refactor logo component to remove unused code

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,6 +1,5 @@
 import { forwardRef } from 'react';
 // @mui
-import { useTheme } from '@mui/material/styles';
 import Link from '@mui/material/Link';
 import Box, { BoxProps } from '@mui/material/Box';
 // routes
@@ -12,54 +11,44 @@ export interface LogoProps extends BoxProps {
   disabledLink?: boolean;
 }
 
-const Logo = forwardRef<HTMLDivElement, LogoProps>(
-  ({ disabledLink = false, sx, ...other }, ref) => {
-    const theme = useTheme();
+const Logo = forwardRef<HTMLDivElement, LogoProps>(({ disabledLink = false, sx }) => {
+  // OR using local (public folder)
+  // -------------------------------------------------------
+  const logo = (
+    <Box
+      component="img"
+      src="/logo/logo_single.svg"
+      sx={{ width: 55, height: 55, cursor: 'pointer', ...sx }}
+    />
+  );
 
-    const PRIMARY_LIGHT = theme.palette.primary.light;
+  // const logo = (
+  //   <Box
+  //     ref={ref}
+  //     component="div"
+  //     sx={{
+  //       width: 40,
+  //       height: 40,
+  //       display: 'inline-flex',
+  //       ...sx,
+  //     }}
+  //     {...other}
+  //   >
+  //     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512">
+  //       Custom svg logo image
+  //     </svg>
+  //   </Box>
+  // );
 
-    const PRIMARY_MAIN = theme.palette.primary.main;
-
-    const PRIMARY_DARK = theme.palette.primary.dark;
-
-    // OR using local (public folder)
-    // -------------------------------------------------------
-    const logo = (
-      <Box
-        component="img"
-        src="/logo/logo_single.svg"
-        sx={{ width: 55, height: 55, cursor: 'pointer', ...sx }}
-      />
-    );
-
-    // const logo = (
-    //   <Box
-    //     ref={ref}
-    //     component="div"
-    //     sx={{
-    //       width: 40,
-    //       height: 40,
-    //       display: 'inline-flex',
-    //       ...sx,
-    //     }}
-    //     {...other}
-    //   >
-    //     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 512 512">
-    //       Custom svg logo image
-    //     </svg>
-    //   </Box>
-    // );
-
-    if (disabledLink) {
-      return logo;
-    }
-
-    return (
-      <Link component={RouterLink} href="/" sx={{ display: 'contents' }}>
-        {logo}
-      </Link>
-    );
+  if (disabledLink) {
+    return logo;
   }
-);
+
+  return (
+    <Link component={RouterLink} href="/" sx={{ display: 'contents' }}>
+      {logo}
+    </Link>
+  );
+});
 
 export default Logo;
